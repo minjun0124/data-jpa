2ee0a152fef858af6e28d045a1f4f2d9c5ffe2c7
mjlee <minjun0124@gmail.com>
2021-07-15 15:25:33 +0900

    feat: domain class converter

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,31 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    /**
+     * Domain class converter
+     *  - 복잡한 경우에는 지양한다.
+     */
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+}
